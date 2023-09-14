494ec9d43e96dc0214ee1277044914de5df39b47
Kyle Kelley <rgbkrk@gmail.com>
2023-09-14 12:09:28 -0700

    include examples and be more stringent

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # display.js
+
 Rich Displays for Jupyter JavaScript Kernels
+
+```typescript
+import { display } from "https://deno.land/x/display/mod.ts"
+display({'text/markdown': 'Get ready for **denotebooks**! ![](https://github.com/denoland.png?size=32)'})
+```
+
+## Background
+
+With Deno's 1.37 release comes a built in Jupyter kernel. You can create notebooks with JavaScript and TypeScript.
+
+One key feature of Jupyter kernels is the ability to display objects as richer media types like HTML, Markdown, Images, Vega, JSON, and many more.
+
+This module provides a simple API to display richer media types in the Deno kernel. Centering on this is the use of a `Symbol.for('Jupyter.display')` on an object to indicate to Deno that the object should be displayed.
+
+```typescript
+{
+  [Symbol.for('Jupyter.display')]: () => ({
+    'text/markdown': '# Hello from Deno'
+  })
+}
+```
+
+Obviously this isn't the most _fun_ interface which is why this module provides a `display` function to make it easier to use.
+
+```typescript
+display({'text/markdown': '# Hello from Deno'})
+```
--- mod.ts
+++ mod.ts
@@ -1,7 +1,5 @@
 export const $display = Symbol.for("Jupyter.display")
 
-type Displayable = unknown;
-
 interface DisplayOptions {
   raw?: boolean
 }
@@ -9,15 +7,23 @@ interface DisplayOptions {
 type MediaBundle = {
   'text/plain'?: string
   'text/html'?: string
+  'image/svg+xml'?: string
   'text/markdown'?: string
+  'application/javascript'?: string
+
+  // Images (sadly, per Jupyter spec) must be base64 encoded. We could _allow_
+  // accepting Uint8Array or ArrayBuffer within `display` calls, however we still
+  // need to encode them for jupyter.
   'image/png'?: string // WISH: Uint8Array | ArrayBuffer
   'image/jpeg'?: string // WISH: Uint8Array | ArrayBuffer
   'image/gif'?: string // WISH: Uint8Array | ArrayBuffer
   'application/pdf'?: string // WISH: Uint8Array | ArrayBuffer
-  'image/svg+xml'?: string
-  'application/javascript'?: string
+
+  // TODO(rgbkrk): File an issue on Deno to support `application/.*json` as objects
+  //               as currently Deno is only sending strings.
   'application/json'?: Object // Note: must be JSON serializable
   'application/geo+json'?: Object
+  'application/vdom.v1+json'?: Object
   'application/vnd.plotly.v1+json'?: Object
   'application/vnd.vega.v5+json'?: Object
   'application/vnd.vegalite.v4+json'?: Object
@@ -25,6 +31,21 @@ type MediaBundle = {
 
   // Must support a catch all for custom mime-types
   [key: string]: string | Object | undefined
+}
+
+type Displayable = {
+  [$display]: () => MediaBundle
+};
+
+function hasDisplaySymbol(obj: unknown): obj is Displayable {
+  return obj !== null && typeof obj === "object" && $display in obj;
+}
+
+function isMediaBundle(obj: unknown, raw: boolean = true): obj is MediaBundle {
+  if (obj !== null && typeof obj === "object") {
+    return raw ? true : Object.keys(obj).every(key => typeof obj[key] === "string");
+  }
+  return false
 }
 
 /**
@@ -36,15 +57,22 @@ type MediaBundle = {
  * @param options - Display options with a default { raw: true }
  * @returns A media bundle object
  */
-export function display(obj: Displayable, options: DisplayOptions = { raw: true }): MediaBundle {
-  if (!options.raw) {
-    throw new Error("Only raw=true is supported at this time");
+export function display(obj: unknown, options: DisplayOptions = { raw: true }): Displayable {
+  // Check to see if the obj already has a Symbol.for("Jupyter.display") method on it
+  // If so, just return it.
+  if (hasDisplaySymbol(obj)) {
+    return obj;
   }
 
-  // If raw is true, return the object as-is; otherwise, you can add additional behavior
-  const displayObject = obj;
+  if (!options.raw) {
+    throw new Error("Only raw=true is supported at this time.");
+  }
 
-  return {
-    [Symbol.for("Jupyter.display")]: () => displayObject
-  };
+  if (isMediaBundle(obj, options.raw)) {
+    return {
+      [$display]: () => obj
+    }
+  }
+
+  throw new Error("When using raw=true, the object must be a collection of media types.")
 }
