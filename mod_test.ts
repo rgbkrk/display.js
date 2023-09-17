72b15a418d9f54c049a7901a28298e50a43d5bb0
Kyle Kelley <rgbkrk@gmail.com>
2023-09-17 13:50:59 -0700

    assert that displaying canvas does the work

--- mod_test.ts
+++ mod_test.ts
@@ -1,6 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
 
+import { createCanvas } from "https://deno.land/x/canvas/mod.ts";
+
 import { display, $display } from "./mod.ts";
+import { assertExists } from "https://deno.land/std@0.201.0/assert/assert_exists.ts";
 
 Deno.test("display() returns a MediaBundle", () => {
   const bundle = display({
@@ -13,5 +16,23 @@ Deno.test("display() returns a MediaBundle", () => {
   assertEquals(result, {
     "image/png": "data:image/png;base64,abc123",
     "text/plain": "hello world",
+  });
+});
+
+Deno.test("display() returns a MediaBundle with a canvas", () => {
+  const canvas = createCanvas(5, 5);
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 5, 5);
+
+  const bundle = display(canvas);
+
+  const result = bundle[$display]();
+
+  assertExists(result["image/png"]);
+
+  assertEquals(result, {
+    "image/png":
+      "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAAXNSR0IArs4c6QAAAARzQklUCAgICHwIZIgAAAAVSURBVAiZY/zPwPCfAQ0woQtQQRAAzqkCCB/D3o0AAAAASUVORK5CYII=",
   });
 });
